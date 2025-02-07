8bbd4cf869c9c1d0c38b0ccb954cb64701d53e37
cian91-hassolthine <hassolthinecian@apps.ipb.ac.id>
2025-02-07 15:01:00 +0700

    Add files via upload
    mengambil nilai matriksnya

---
+++ MengambilNilaiMatriks.py
@@ -0,0 +1,5 @@
+import cv2
+five= cv2.imread("d:/Studi Doctoral/Semester 2/Pengenalan Pola/Praktikum/Praktikum 2/5.png")  
+# acces pixel of images per postion 
+pixels = five[100,100]
+print(pixels)
