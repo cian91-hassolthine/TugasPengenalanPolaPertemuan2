a08ff7c13042e73ae6e088cd94143c5cd7a5774b
cian91-hassolthine <hassolthinecian@apps.ipb.ac.id>
2025-02-07 14:17:01 +0700

    Add files via upload
    Membaca data dari file dengan format CSV

---
+++ MembacaFileCSV.py
@@ -0,0 +1,4 @@
+# Membaca data dari file dengan format CSV
+import pandas as pd
+data = pd.read_csv("d:/Studi Doctoral/Semester 2/Pengenalan Pola/Praktikum/Praktikum 2/Data.csv", sep=";")
+print(data)
