dd6d22dbf459287167898ee64073bda1355b2799
cian91-hassolthine <hassolthinecian@apps.ipb.ac.id>
2025-02-07 14:25:17 +0700

    Add files via upload
    Membaca data dari file dengan format text (delimeter)

---
+++ Datadelimiter.py
@@ -0,0 +1,7 @@
+
+# Membaca data dari file dengan format text (delimeter)
+print("\n read text data with tab delimiter") 
+with open ('d:/Studi Doctoral/Semester 2/Pengenalan Pola/Praktikum/Praktikum 2/Data.txt') as data:
+  print(data.read()) 
+
+
